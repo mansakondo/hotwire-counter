5f23c45216f2e254b7587dc836aafaf0d61a9880
mansakondo <mansakondo22@gmail.com>
2022-07-17 14:30:18 +0200

    Add `#increment` and `#decrement` actions

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,4 +1,32 @@
 class CounterController < ApplicationController
   def show
+    render :show, locals: { count: count.value }
   end
+
+  def increment
+    count.increment
+
+    broadcast
+
+    redirect_to root_url
+  end
+
+  def decrement
+    count.decrement
+
+    broadcast
+
+    redirect_to root_url
+  end
+
+  private
+    def count
+      Kredis.counter "global:counter:count"
+    end
+
+    def broadcast
+      Turbo::StreamsChannel.broadcast_update_later_to "global:counter",
+        target: "global-count",
+        content: count.value
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "counter#show"
+
+  post "/increment", to: "counter#increment", as: :increment
+  post "/decrement", to: "counter#decrement", as: :decrement
 end
